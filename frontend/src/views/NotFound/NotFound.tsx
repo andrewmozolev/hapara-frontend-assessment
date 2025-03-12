abca7d3d253106c217b09ac3edcdce50da71d1dd
Andrew Mozolev <andrew.mozolev@gmail.com>
2025-03-12 17:24:38 +1300

    Add NotFound component

---
+++ frontend/src/views/NotFound/NotFound.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-xl font-bold">Sorry, this page is not found!</h1>
+    </div>
+  );
+}
